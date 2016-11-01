00806f06160db5b2aef9e2258c60e49ca9f8b6f4
Matt Parrella <parrellam617@strose.edu>
2016-11-01 15:42:59 -0400

    Add files via upload

--- Card.java
+++ Card.java
@@ -1,16 +1,45 @@
+import java.nio.charset.Charset;
 import java.util.Scanner;
 
 
 public class Card {
 
+	private int value;
 	private static String card;
 	public static int count = 0;
+	private static int aceVal;
 
 	private static final String[] Suit = {"void", "H", "S", "C", "D"};
 	private static final String[] Cards = { "void", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 	public static String[] usedCards = new String[52];
 
-	
+
+	public Card(){
+
+		value = 0;
+		card = null;
+	}
+
+	public void setCard(String card){
+
+		this.card = card;
+	}
+
+	public String getCard(){
+
+		return card;
+	}
+
+	public void setValue(int value){
+
+		this.value = value;
+	}
+
+	public int getValue(){
+
+		return value;
+	}
+
 	public static String randomCard(){
 
 		String card = null;
@@ -49,17 +78,31 @@ public class Card {
 
 	public static String addToDeck(){
 		boolean flag = true;
+		
 		while(flag == true){
 			card = randomCard();
 			if (isDrawn(card)==false){
-				usedCards[count] = card;
-				count++;
-				flag = false;
+				if (card.charAt(0) == 'A'){
+					Card.setAce();
+					usedCards[count] = card;
+					count++;
+					flag = false;
+				}
+				else{
+					
+					usedCards[count] = card;
+					count++;
+					flag = false;
+				
+				}
+				
 			}
 		}
 		
 		return card;
 	}
+
+
 
 	public static int assignValue(String card){
 
@@ -75,28 +118,34 @@ public class Card {
 				val = Character.getNumericValue(c);
 			}
 			else if (c == 'A'){
-				Scanner kb = new Scanner(System.in);
-				System.out.println("Please choose the value for Ace (1 or 11): ");
-				String userChoice = kb.nextLine();
-				if (userChoice.equals("1")){
-					val = 1;
-				}
 				
-				else if (userChoice.equals("11")){
-				val = 11;
-				}
-				
-				else {
-					System.out.println("Error: Please enter either 1 or 11.");
-					System.exit(0);
-				}
-				
+				val = aceVal;
 			}
 			else if (Character.isLetter(c) == true){
 				val = 10;
 			}
 		}
 		return val;
+	}
+	
+	private static void setAce(){
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please choose the value for Ace (1 or 11): ");
+		int userChoice = kb.nextInt();
+		
+		if (userChoice == 1 || userChoice == 11){
+			aceVal = userChoice;
+			
+		}
+		
+		else{
+			System.out.println("Error: Please enter either 1 or 11.");
+			System.exit(0);
+		
+			
+		}
+		
+		
 	}
 
 
